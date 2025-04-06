c3f6fa8acfcafb1a8139ac66e45a6975399f10cf
Blair Kitchen <blair@blairkitchen.com>
2025-04-06 14:10:44 -0400

    Making test files executable and adding a runtests.tcl script to run all tests

---
+++ runtests.tcl
@@ -0,0 +1,14 @@
+#!/usr/bin/tclsh
+# Copyright (c) 2025, Blair Kitchen
+# All rights reserved.
+#
+# See the file "license.terms" for informatio on usage and redistribution
+# of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+
+package require Tcl 8.5
+package require tcltest 2.3
+
+::tcltest::configure -testdir [file dirname [file normalize [info script]]]
+::tcltest::configure {*}$argv
+
+::tcltest::runAllTests
